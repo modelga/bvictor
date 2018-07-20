6b0770258fe4d84e20769da69f4e43906123e299
Mateusz Odelga <mateusz.odelga@schibsted.pl>
2018-07-21 00:36:56 +0200

    Set-up code coverage and integration testing

---
+++ app/app.test.js
@@ -0,0 +1,17 @@
+const test = require('ava');
+const supertest = require('supertest');
+const app = require('./app');
+
+const proxiedBetVictorConfig = {
+  get(key) {
+    if (key === 'UPSTREAM_BASE_URL')
+      return 'https://betvictor-proxy.herokuapp.com';
+    throw new Error('Invalid key');
+  }
+};
+
+test('should return list of sports', async t => {
+  const server = supertest(app(proxiedBetVictorConfig));
+  const data = await server.get('/').expect(200);
+  t.pass();
+});
