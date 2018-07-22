d8c9038024e841d58c56cf31e5ce73a62e1beae7
Mateusz Odelga <mateusz.odelga@schibsted.pl>
2018-07-22 23:27:01 +0200

    Cleanup package.json and fix typo in eslint disable

--- app/error/index.js
+++ app/error/index.js
@@ -4,7 +4,7 @@ module.exports = {
   exceptions: {
     NotFound
   },
-  // eslint-disable-next-line no-unused-varsd
+  // eslint-disable-next-line no-unused-vars
   handler(err, req, res, next) {
     if (err instanceof NotFound) {
       return res.status(404).send(`Not Found: ${err.message}`);
