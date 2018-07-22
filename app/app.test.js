8e247a1b43dc0b2af125c1e959d65d54c7ad346a
Mateusz Odelga <mateusz.odelga@schibsted.pl>
2018-07-23 00:54:15 +0200

    Set-up redis on travis. Add test to use redis

--- app/app.test.js
+++ app/app.test.js
@@ -200,6 +200,24 @@ test('should use redis cache fallback', async t => {
   t.pass();
 });
 
+test('should use redis if defined', async t => {
+  if (!process.env.USE_REDIS) {
+    t.pass(); // skip test if redis is not in use
+    return;
+  }
+  const config = {
+    get(key) {
+      if (key === 'CACHE_ENGINE') return 'redis';
+      if (key === 'CACHE_TTL_SECONDS') return 1;
+      if (key === 'REDIS_URL') return process.env.USE_REDIS;
+      return baseConfig.get(key);
+    }
+  };
+  const server = supertest(app(config));
+  await server.get('/en-gb/sports').expect(200);
+  t.pass();
+});
+
 test('should use no-cache', async t => {
   const config = {
     get(key) {
