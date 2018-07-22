d3e70676c83949ed99d20f0847b6f39ca2f94f4a
Mateusz Odelga <mateusz.odelga@schibsted.pl>
2018-07-22 20:49:18 +0200

    Stop app after exhausted connection retries

--- app/cache/redis.js
+++ app/cache/redis.js
@@ -3,7 +3,7 @@ const URL = require('url');
 const debug = require('debug');
 const R = require('ramda');
 
-const log = debug('bet-victor:sports-live:cache');
+const log = debug('bet-victor:sports-live:redis');
 
 const RedisCache = (db, dataFn, ttl) => async key => {
   const data = await db.rawCallAsync(['GET', key || 'cached']);
@@ -25,8 +25,16 @@ module.exports = (config, dataFn, fallback = dataFn) => {
   const db = new Redis({
     host: connectionUrl.hostname,
     port: connectionUrl.port,
-    auth: connectionUrl.auth && connectionUrl.auth.split(':')[1]
+    auth: connectionUrl.auth && connectionUrl.auth.split(':')[1],
+    maxRetries: 5
   });
+  db.on('error', e => {
+    if (e.message.indexOf('exhausted retries') !== -1) {
+      throw e;
+    }
+    log('Redis Error', e);
+  });
+
   const ttl = config.get('CACHE_TTL_SECONDS');
   const redisCache = RedisCache(db, dataFn, ttl);
   return key => {
